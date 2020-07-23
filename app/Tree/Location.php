b5ef4197e5b98c2a0dff79cf2db8389be94b79dc
Marco Antoni <marco.antoni910@gmail.com>
2020-07-23 20:32:19 -0300

    Pegando a localizacao (UF) com base no IP

--- app/Tree/Location.php
+++ app/Tree/Location.php
@@ -5,10 +5,11 @@ namespace App\Tree;
 use App\Tree\Branch;
 use App\Tree\DecisionTree;
 use Closure;
-use Google\Cloud\Language\V1\Entity\Type as EntityType;
 use App\Municipio;
 use App\UF;
+use Google\Cloud\Language\V1\Entity\Type as EntityType;
 
+use Request;
 
 class Location extends Branch {
 
@@ -18,25 +19,41 @@ class Location extends Branch {
         $uf = null;
         $condition = [];
         $lookingState = false;
+        // variavel de controle para saber se foi especificado uma localizacao (uf) dentro da busca.
+        // Se nao encontrou uma uf, e preciso buscar a localizacao
+        
+        $ufPresent = false;
 
+        // melhor caso: usuario informou cidade e uf
         foreach ($tree->getEntityies() as $entity) {
            if ($entity->getType() == EntityType::LOCATION) {
                 $entityName = $entity->getName();
                               
                 if (strlen($entityName) == 2){
                     $uf = $entityName;
+                    $ufPresent = true;
                 } else {
                     $city = $entityName;
                 }
             }
         }
 
+        // se nao encontrou um estado irá buscar pela localizacao do IP
+        if (!$ufPresent) {
+            $uf = $this->getLocalizationByIP();
+        }
+        
         if ($city == null){
             foreach ($tree->getEntityies() as $entity) {
-               if ($entity->getType() == EntityType::OTHER) {
-                    $entityName = $entity->getName();
-                    $city = $entityName;
-                    break;
+                $query = Municipio::where('uf', '=', $uf)->where('nome', 'like', '%'.$entity->getName().'%')->first(); 
+                $search = $query['NOME'];
+                // se a consulta retornou resultado
+                if ($query) {
+                    // testa se o nome da cidade está presente na sentenca
+                    if(preg_match("/{$search}/i", $tree->sentence)) {
+                        $city = $search;
+                        break;
+                    }   
                 }
             }
         }
@@ -69,7 +86,22 @@ class Location extends Branch {
         } 
 
         $tree->addCondition($condition);  
-      
+        
         return $next($tree);
+    }
+
+    public function getLocalizationByIP(){
+        $ip = '170.244.220.26';
+        // documentacao -> https://ip-api.com/docs/api:json
+        $url = 'http://ip-api.com/json/' . $ip . '?fields=region';
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);  
+        $data = json_decode($output);
+
+        return $data->region;   
     }
 }
