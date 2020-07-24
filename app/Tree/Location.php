437eaa18a6eec0972269050522e7fd63c17b9821
Marco Antoni <marco.antoni910@gmail.com>
2020-07-24 15:20:21 -0300

    Melhoria na busca de localidades

--- app/Tree/Location.php
+++ app/Tree/Location.php
@@ -8,68 +8,59 @@ use Closure;
 use App\Municipio;
 use App\UF;
 use Google\Cloud\Language\V1\Entity\Type as EntityType;
-
 use Request;
 
 class Location extends Branch {
 
     function handle(DecisionTree $tree, Closure $next): DecisionTree {
      
-        $city = null;
-        $uf = null;
+        $cityId = null;
         $condition = [];
+        $foundCity = false;
         $lookingState = false;
+        $reseultSet = null;
+        $uf = null;
+        $ufId = null;
+        
         // variavel de controle para saber se foi especificado uma localizacao (uf) dentro da busca.
         // Se nao encontrou uma uf, e preciso buscar a localizacao
-        
         $ufPresent = false;
-
-        // melhor caso: usuario informou cidade e uf
+        
+        /* No melhor caso, o foi digitado a cidade e UF. Primeiro procura a UF */
         foreach ($tree->getEntityies() as $entity) {
            if ($entity->getType() == EntityType::LOCATION) {
-                $entityName = $entity->getName();
-                              
+                $entityName = $entity->getName();    
                 if (strlen($entityName) == 2){
                     $uf = $entityName;
                     $ufPresent = true;
-                } else {
-                    $city = $entityName;
-                }
+                } 
             }
         }
 
-        // se nao encontrou um estado irá buscar pela localizacao do IP
+        /* Se nao encontrou uma UF, irá buscar pela localizacao do IP para restringir a busca do municipio */
         if (!$ufPresent) {
             $uf = $this->getLocalizationByIP();
         }
-        
-        if ($city == null){
-            foreach ($tree->getEntityies() as $entity) {
-                $query = Municipio::where('uf', '=', $uf)->where('nome', 'like', '%'.$entity->getName().'%')->first(); 
+
+        // procura o municipio
+        foreach ($tree->getEntityies() as $entity) {
+            $query = Municipio::where('uf', '=', $uf)->where('nome', 'like', '%'.$entity->getName().'%')->first();
+            // se a consulta retornou um resultado, testa se a expressao está presente na sentenca
+            if ($query) {
                 $search = $query['NOME'];
-                // se a consulta retornou resultado
-                if ($query) {
-                    // testa se o nome da cidade está presente na sentenca
-                    if(preg_match("/{$search}/i", $tree->sentence)) {
-                        $city = $search;
-                        break;
-                    }   
+                if(preg_match("/{$search}/i", $tree->sentence)) {
+                    $cityId = $query['CO_MUNICIPIO'];
+                    $foundCity = true;
+                    break;
                 }
-            }
+            }   
         }
             
         // se nao encontrou uma cidade, a pesquisa é pelo estado
-        if ($city == null) {
+        if (! $cityId) {
             $reseultSet = UF::where('NO_UF', '=', $uf)->first();
             $lookingState = true; 
-        } elseif ($uf) {
-            $reseultSet = Municipio::where([
-                ['nome', '=', $city],
-                ['uf', '=', $uf]
-            ])->first();
-        } else {
-            $reseultSet = Municipio::where('nome', $city)->first();
-        }
+        } 
  
         if ($lookingState){
             $condition = array(
@@ -81,7 +72,7 @@ class Location extends Branch {
             $condition = array(
                'field' => 'CO_MUNICIPIO',
                 'operator' => '=',
-                'value' => $reseultSet['id']
+                'value' => $cityId
             );
         } 
 
@@ -89,12 +80,19 @@ class Location extends Branch {
         
         return $next($tree);
     }
-
+    
+    /**
+    *
+    * Busca a Unidade Federativa a que um IP pertence
+    * Faz uma requisicao para a API  ip-api
+    * @see      https://ip-api.com/docs/api:json
+    * @author   Marco Antoni <marco.antoni910@gmail.com>
+    * @return   String
+    *
+    */
     public function getLocalizationByIP(){
-        $ip = '170.244.220.26';
-        // documentacao -> https://ip-api.com/docs/api:json
+        $ip = '170.244.220.26'; // Request::ip()
         $url = 'http://ip-api.com/json/' . $ip . '?fields=region';
-        
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
