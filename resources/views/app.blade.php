d0d0c648b3ceeae0e333a8ca2abe36530fd949dd
Marco Antoni <marco.antoni910@gmail.com>
2020-07-31 16:57:49 -0300

    Deixando o js do analytics fixo para facilitar o deploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -166,6 +166,15 @@
         });
       });
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171693860-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-171693860-1');
+    </script>
   </div>
 </body>
 </html>
