1b3fc70220aae66d9369410bb36b4700bee67333
Marco Antoni <marco.antoni910@gmail.com>
2020-08-02 21:35:50 -0300

    Rewrite

--- app/Tree/Student/TransportType.php
+++ app/Tree/Student/TransportType.php
@@ -12,71 +12,67 @@ class TransportType extends Branch {
 	
 	function handle(DecisionTree $tree, Closure $next): DecisionTree {
 		
-		$condition = false;
+		$match = false;
+		$field = '';
+		$value = 1;
 		$schoolsFound = session('schoolsFound');
 		$messageTransport = false;
 		
 		if (preg_match('/public/', $tree->sentence)) {
-	        $condition = array(
-	           'field' => 'IN_TRANSPORTE_PUBLICO',
-	            'operator' => '=',
-	            'value' => 1
-	        );
+	        $match = true;
+	        $field = 'IN_TRANSPORTE_PUBLICO';
 	        $messageTransport = 'que usam transporte público';
 		}
 
-		if (preg_match(('/bicicleta/'), $tree->sentence)){
-	        $condition = array(
-	           'field' => 'IN_TRANSP_BICICLETA',
-	            'operator' => '=',
-	            'value' => 1
-	        );
+		if (preg_match('/bicicleta/', $tree->sentence)){
+	        $match = true;
+	        $field = 'IN_TRANSP_BICICLETA';
 	        $messageTransport = 'que usam bicicleta';
 		}
 
-		if (preg_match(('/onibus/'), $tree->sentence)){
-	        $condition = array(
-	           'field' => 'IN_TRANSP_ONIBUS',
-	            'operator' => '=',
-	            'value' => 1
-	        );
+		if (preg_match('/onibus/', $tree->sentence)){
+	        $match = true;
+	        $field = 'IN_TRANSP_ONIBUS';
 	        $messageTransport = 'que usam ônibus';
 		}
 
-		if (preg_match(('/animal/'), $tree->sentence)){
-	        $condition = array(
-	           'field' => 'IN_TRANSP_TR_ANIMAL',
-	            'operator' => '=',
-	            'value' => 1
-	        );
+		if (preg_match('/animal/', $tree->sentence)){
+	        $match = true;
+	        $field = 'IN_TRANSP_TR_ANIMAL';
 	        $messageTransport = 'que usam transporte de tração animal';
 		}
 
-		if ($condition){
+		if (preg_match('/vam/', $tree->sentence) || preg_match('/kombi/', $tree->sentence)){
+	        $match = true;
+	        $field = 'IN_TRANSP_VANS_KOMBI';
+	        $messageTransport = 'que usam vam/kombi';
+		}
+
+		if ($match){
 			if ($schoolsFound == 0 || $schoolsFound == 1){
-				$tree->setQuery($tree->getQuery()->where($condition['field'], $condition['value']));
+				$tree->setQuery($tree->getQuery()->where($field, $value));
 			} else {
 				// Percorre o array $data da classe Answer
 				// O indíce [0] armazena o nome da escola enquanto o [1] armazena o objeto da classe Builder que representa o número de alunos
 				// Basta adicionar a restrição a consulta 
 				foreach ($tree->answer->data as $key => $value) {
 					//print_r($value);
-					$value[1]->where($condition['field'], $condition['operator'], $condition['value']);
+					$value[1]->where($field, $value);
 				}
 			}
 
 			// Adiciona a condição do transporte ao objeto builder para para gerar as estatísticas do meio de transporte utilizado
 			if (session('schoolsFound') > 0){
-				$tree->answer->statistics->query['city']['cache']->where($condition['field'], $condition['operator'], $condition['value']);
-				$tree->answer->statistics->query['city']['nocache']->where($condition['field'], $condition['operator'], $condition['value']);
+				$tree->answer->statistics->query['city']['cache']->where($field, $value);
+				$tree->answer->statistics->query['city']['nocache']->where($field, $value);
 			}
 
 			if (session('CO_MUNICIPIO')) {
-				$tree->answer->statistics->query['state']['cache']->where($condition['field'], $condition['operator'], $condition['value']);
-				$tree->answer->statistics->query['state']['nocache']->where($condition['field'], $condition['operator'], $condition['value']);
+				$tree->answer->statistics->query['state']['cache']->where($field, $value);
+				$tree->answer->statistics->query['state']['nocache']->where($field, $value);
 			}
 
-			$tree->answer->statistics->fields[$condition['field']] = $condition['value'];
+			$tree->answer->statistics->fields[$field] = $value;
 		}
 
 		session(['messageTransport' => $messageTransport ]);
