0f2da9407a1eb828bedfe007cd1d9e359332c17a
Marco Antoni <marco.antoni910@gmail.com>
2020-10-22 09:58:08 -0300

    Correção ortográfica MS API

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -44,11 +44,13 @@ class SearchController extends Controller {
     
     public function store(Request $request) {
         /* Grava as perguntas no BD */;
-        #$log = new Log();
-        #$log->sentence = $request->input('search');
-        #$log->save();
+        //$log = new Log();
+        //$log->sentence = $request->input('search');
+        //$log->save();
+        
+        $sentence = $this->speelCheck($request->input('search'));
         // inicia o processamento
-        $decision_tree = new DecisionTree($request->input('search'));
+        $decision_tree = new DecisionTree($sentence);
         $analyse = $decision_tree->analyze();
         
         $answer = $decision_tree->process();
@@ -62,6 +64,52 @@ class SearchController extends Controller {
             'stats' => $answer->statistics->stats,
             'userMessage' => $answer->getUserMessage()
         ]);
+    }
+
+    public function speelCheck($input){
+        $host = 'https://speelcheckdissertacao.cognitiveservices.azure.com';
+        $path = '/bing/v7.0/spellcheck?';
+        $params = 'mkt=pt-BR&mode=proof';
+
+        $data = array (
+            'text' => urlencode ($input)
+        );
+
+        $key = '8d5decd4544f4315a1e77c0fe78e6427';
+
+        $headers = "Content-type: application/x-www-form-urlencoded\r\n" .
+            "Ocp-Apim-Subscription-Key: $key\r\n";
+
+        $options = array (
+            'http' => array (
+                'header' => $headers,
+                'method' => 'POST',
+                'content' => http_build_query ($data)
+            )
+        );
+
+        $context  = stream_context_create ($options);
+        $result = file_get_contents ($host . $path . $params, false, $context);
+
+        $json = json_encode(json_decode($result), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        $obj = json_decode($json, TRUE);
+
+        foreach ($obj['flaggedTokens'] as $key => $value) {
+            $token = $value['token'];;
+            $suggestion = '';
+
+            foreach ($value['suggestions'] as $key => $value) {
+                $suggestion = strval($value['suggestion']);
+                // pega sempre a primeira sugestão, cujo score é o maior
+                if ($key == 0){
+                    $input = str_replace ($token, $suggestion, $input); 
+                    break;
+                }
+            }
+        }
+
+       return $input;
+
     }
 
 }
